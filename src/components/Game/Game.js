b18c6cbf7497e928ae9853ae3ff707e3801756b8
quay189 <34814999+quay189@users.noreply.github.com>
2020-05-02 10:50:36 +0700

    Add event Space

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Bird from "../Bird/Bird";
 import Foreground from "../Foreground/Foreground";
 import Pipe from "../Pipe/Pipe";
 
 import "./Game.css";
 const Game = () => {
+  useEffect(() => {
+    const handleKeyPress = (e) => {
+      if (e.keyCode === 32) {
+        fly();
+      }
+    };
+    document.addEventListener("keypress", handleKeyPress);
+  }, []);
   return (
     <div className="Game">
       <Bird />
@@ -12,6 +20,10 @@ const Game = () => {
       <Foreground />
     </div>
   );
+};
+
+const fly = () => {
+  console.log("fly");
 };
 
 export default Game;
